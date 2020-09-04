2ad7a7ee3871bb20e97d44e2867d87e242f0b15e
karyRmartinez <karymartinez@pursuit.org>
2020-09-04 11:45:35 -0400

    navigation tabViewController

---
+++ Artspace2.0/ViewControllers/uploadImageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  uploadImageViewController.swift
+//  Artspace2.0
+//
+//  Created by Kary Martinez on 9/4/20.
+//  Copyright © 2020 Artspace. All rights reserved.
+//
+
+import UIKit
+
+
+class AddImageViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+     
+    }
+    
+
+}
+
