e0544cf454e8b6ebad45ed055675e18ee8d3a451
karyRmartinez <karymartinez@pursuit.org>
2020-09-08 13:37:36 -0400

    initial commit to DetailViewController

---
+++ Artspace2.0/ViewControllers/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Artspace2.0
+//
+//  Created by Kary Martinez on 9/8/20.
+//  Copyright © 2020 Artspace. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        
+    }
+
+}
