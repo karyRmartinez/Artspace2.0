fb25bc84bfcff35c19cea84224fdbf0d9944679a
karyRmartinez <karymartinez@pursuit.org>
2020-10-05 18:28:13 -0400

    pod installation

---
+++ ArtSpace-iOS/ArtSpace-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtSpace-iOS
+//
+//  Created by Kary Martinez on 10/5/20.
+//  Copyright © 2020 Kary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
