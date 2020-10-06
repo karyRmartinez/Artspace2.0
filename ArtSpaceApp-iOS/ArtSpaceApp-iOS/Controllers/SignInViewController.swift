d8c4955260aed66d813e2cb48bba2603116b6072
karyRmartinez <karymartinez@pursuit.org>
2020-10-06 15:02:49 -0400

    added new logInViewController file

---
+++ ArtSpaceApp-iOS/ArtSpaceApp-iOS/Controllers/SignInViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignInViewController.swift
+//  ArtSpaceApp-iOS
+//
+//  Created by Kary Martinez on 10/6/20.
+//  Copyright © 2020 Kary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class logInViewController: UIViewController {
+    
+  
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+      
+    }
+   
+ 
+}
