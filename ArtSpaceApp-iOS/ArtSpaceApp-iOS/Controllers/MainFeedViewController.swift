ffe843e81ad521b3e4d6d91b49c71fade2d9e371
karyRmartinez <karymartinez@pursuit.org>
2020-11-11 13:12:12 -0500

    testing commit

--- ArtSpaceApp-iOS/ArtSpaceApp-iOS/Controllers/MainFeedViewController.swift
+++ ArtSpaceApp-iOS/ArtSpaceApp-iOS/Controllers/MainFeedViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class MainFeedViewController: UIViewController {
-
+    
+    
+    lazy var artCollectionView: UICollectionView = {
+      let layout = UICollectionViewFlowLayout.init()
+      let collectionView = UICollectionView(frame:.zero , collectionViewLayout: layout)
+      layout.scrollDirection = .vertical
+      layout.itemSize = CGSize(width: 250, height: 250)
+      return collectionView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
